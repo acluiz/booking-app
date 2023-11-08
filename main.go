708c3fee96fd2dcc993deef69acb7fbeb4d85ffe
Ana Luiz <anamurdock881@gmail.com>
2023-11-07 21:36:47 -0300

    feat: get infos from user

--- main.go
+++ main.go
@@ -11,4 +11,23 @@ func main() {
 	fmt.Printf("Welcome to %s booking application! \n", conferenceName)
 	fmt.Printf("We have total of %d tickets and %d are still available. \n", remainingTickets, totalTickets)
 	fmt.Println("Get your tickets here to attend")
+
+	var firstName string
+	var lastName string
+	var email string
+	var userTickets uint
+
+	fmt.Println("Enter your first name:")
+	fmt.Scan(&firstName)
+
+	fmt.Println("Enter your last name:")
+	fmt.Scan(&lastName)
+
+	fmt.Println("Enter your email address:")
+	fmt.Scan(&email)
+
+	fmt.Println("Enter the number of tickets you want:")
+	fmt.Scan(&userTickets)
+
+	fmt.Printf("Thank you %s %s for booking %d tickets. You will receive a confirmation email at %s\n", firstName, lastName, userTickets, email)
 }
